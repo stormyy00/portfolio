b4f80a6c762d5d2d28bf3dc86f4c0876ba590598
stormyy00 <jonathan.trujillo0432@gmail.com>
2023-12-18 02:43:52 -0800

    updated configs

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 
 const Navigation = () => {
-  return (
-  <div className="flex justify-center">Navigation
-  </div>
-  );
+  return <div className="flex justify-center">Navigation</div>;
 };
 
 export default Navigation;

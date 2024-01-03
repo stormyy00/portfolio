a22cc2c6b34b0d74d8dd06fdc779f8c2b34ee962
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-01-03 01:21:41 -0800

    working lint

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,14 +2,9 @@
 import Navigation from "@/components/Navigation";
 import "./globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Antic_Didone, Montserrat } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import Footer from "@/components/Footer";
 
-const anticDidone = Antic_Didone({
-  subsets: ["latin"],
-  variable: "--font-anticDidone",
-  weight: ["400"],
-});
 const montserrat = Montserrat({
   subsets: ["latin"],
   variable: "--font-montserrat",
@@ -25,7 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${anticDidone.variable} ${montserrat.variable}`}
+      className={` ${montserrat.variable}`}
     >
       <body>
         <div className="bg-web-black w-full flex items-center justify-center flex-col overflow-hidden">

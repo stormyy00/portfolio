9074c5ff8ad2644a8de03b769b506149a115d278
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-02-06 00:34:36 -0800

    added correct links

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,34 +4,34 @@ export const projects = [
     description:
       "Leading a group of 6 people to create a website for UCR's Art Factory Club",
     techStack: ["Next.Js", "Tailwind", "Figma", "Github Actions"],
-    link: "https://github.com/acm-ucr/hackathon-website",
+    link: "https://github.com/stormyy00",
   },
   {
     title: "Gals Press",
     description: "Embedded System using na Arduino Microcontroller",
     techStack: ["Next.Js", "Tailwind", "Flask", "Google Colab"],
-    link: "https://github.com/acm-ucr/hackathon-website",
+    link: "https://github.com/rfairooz/GalsPress",
   },
   {
     title: "R'Tools",
     description:
       "A site where students can run the essential algorithms with visualizations and logical expression",
     techStack: ["Next.js", "Tailwind", "Prettier"],
-    link: "https://github.com/acm-ucr/hackathon-website",
+    link: "https://github.com/acm-ucr/r-tools",
   },
   {
     title: "Design Verse",
     description: "Hackathon catered towars UI/UX designers",
     techStack: ["Next.js", "Tailwind", "Cypress", "Prettier"],
     //   imagePath: require.context("../../public/projects/hackathon", true),
-    link: "https://github.com/acm-ucr/hackathon-website",
+    link: "https://github.com/designatucr/designverse2024",
   },
   {
     title: "PC Cooling System",
     description: "Embedded System using a Arduino Microcontroller",
     techStack: ["Arduino", "C++"],
     //   imagePath: require.context("../../public/projects/hackathon", true),
-    link: "https://github.com/acm-ucr/hackathon-website",
+    link: "https://github.com/stormyy00/PC-Cooling-System",
   },
   {
     title: "Hackathon Website",
@@ -50,12 +50,12 @@ export const projects = [
     title: "Phi Alpha Delta",
     description: "Embedded System using na Arduino Microcontroller",
     techStack: ["Next.js", "Tailwind", "Cypress", "Prettier"],
-    link: "https://github.com/acm-ucr/hackathon-website",
+    link: "https://github.com/acm-ucr/pad-website",
   },
   {
     title: "Highlander Statistics Society",
     description: "Embedded System using na Arduino Microcontroller",
     techStack: ["Next.js", "Tailwind", "Prettier"],
-    link: "https://github.com/acm-ucr/hackathon-website",
+    link: "https://github.com/acm-ucr/hiss-website",
   },
 ];

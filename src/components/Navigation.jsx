3570e985cfcf1d4d96a3231ead3be7ca89bb8e98
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-02-10 18:04:26 -0800

    added tarnsitions

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -19,7 +19,7 @@ const Navigation = () => {
   };
 
   return (
-    <div className="w-full ml-3 py-2 md:mt-72">
+    <div className="w-full ml-3 py-2 md:mt-64">
       <div
         onMouseEnter={toggleDropdown}
         onMouseLeave={closeNavbar}
@@ -29,8 +29,8 @@ const Navigation = () => {
           onClick={toggleDropdown}
           className={`${
             isDropdownOpen
-              ? "opacity-0 duration-300"
-              : "opacity-100 bg-green-500 duration-300"
+              ? "opacity-0 "
+              : "opacity-100 bg-green-500 duration-800"
           } bg-design-green-300 rounded-xl w-1/3 pl-3.5 py-2.5 drop-shadow-xl`}
         >
           <FaBars className="text-white text-lg drop-shadow-md " />
@@ -39,8 +39,8 @@ const Navigation = () => {
           className={`${
             isDropdownOpen
               ? "transition-all duration-300 ease-out transform translate-y-0 opacity-100"
-              : "transition-all duration-300 ease-in  transform -translate-x-96 opacity-0"
-          } bg-green-500 p-2 ml-0 -mt-10 md:-mt-20 grid grid-col rounded-xl drop-shadow-lg`}
+              : "transition-all duration-300 ease-in  transform -translate-y-32 -translate-x-32 opacity-0"
+          } bg-green-500 p-2 ml-0 -mt-10 md:-mt-10 grid grid-col rounded-xl drop-shadow-lg`}
         >
           <button onClick={closeNavbar}>
             <FaChevronLeft className="flex text-white text-xl items-center justify-start my-1 ml-1" />

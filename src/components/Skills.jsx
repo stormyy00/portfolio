1a4be487f244eb051271e42c39c98d291fb9eb00
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-04-11 01:19:19 -0700

    tempory fix to colors

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import skills from "@/data/icons";
+// import SKILLS from "@/data/icons";
 // import Image from "next/image";
 
 const Skills = () => {
@@ -9,7 +9,7 @@ const Skills = () => {
   return (
     <div className="flex flex-col  w-full">
       <div className="flex justify-center flex-col md:flex-row w-auto md:flex-cols md:gap-20 gap-6 mt-2 mb-10">
-        {Object.keys(skills).map((type, index) => (
+        {Object.keys(SKILLS).map((type, index) => (
           <div key={index}>
             <button
               className={`text-3xl font-bold ${
@@ -25,7 +25,7 @@ const Skills = () => {
 
       <div className="grid md:flex grid-cols-4 md:flex-row gap-4 justify-center w-auto md:relative">
         {selectedType &&
-          skills[selectedType].map((techStack, index) => (
+          SKILLS[selectedType].map((techStack, index) => (
             <div
               onMouseEnter={() => setHoveredTech(techStack)}
               onMouseLeave={() => setHoveredTech(null)}
@@ -46,6 +46,102 @@ const Skills = () => {
 };
 
 export default Skills;
+
+import { SiJavascript } from "react-icons/si";
+import { TbBrandNextjs } from "react-icons/tb";
+import { DiJava } from "react-icons/di";
+import { FaPython } from "react-icons/fa";
+import { SiTypescript } from "react-icons/si";
+import { PiFileSqlDuotone } from "react-icons/pi";
+import { SiTailwindcss } from "react-icons/si";
+
+import { SiFirebase } from "react-icons/si";
+import { SiPrisma } from "react-icons/si";
+import { SiReact } from "react-icons/si";
+import { SiFigma } from "react-icons/si";
+import { SiPostman } from "react-icons/si";
+import { TbBrandFramerMotion } from "react-icons/tb";
+import { SiCplusplus } from "react-icons/si";
+
+import { SiCypress } from "react-icons/si";
+import { SiFlask } from "react-icons/si";
+import { SiAdobe } from "react-icons/si";
+// import { FaGit } from "react-icons/fa";
+// import { FaNodeJs } from "react-icons/fa";
+// import { SiOpenai } from "react-icons/si";
+// import { SiGoogleanalytics } from "react-icons/si";
+
+const SKILLS = {
+  Programming: [
+    {
+      name: "C++",
+      icon: <SiCplusplus className="text-tech-cpp" />,
+    },
+    {
+      name: "Python",
+      icon: <FaPython className="text-tech-python" />,
+    },
+    // "HTML/CSS",
+    {
+      name: "Typescript",
+      icon: <SiTypescript className="text-tech-typescript" />,
+    },
+    {
+      name: "Javascript",
+      icon: <SiJavascript className="text-tech-javascript" />,
+    },
+    {
+      name: "Java",
+      icon: <DiJava className="text-6xl -mt-1 text-red-500" />,
+    },
+    {
+      name: "SQL",
+      icon: <PiFileSqlDuotone className="text-tech-cpp" />,
+    },
+  ],
+  "Web Development": [
+    {
+      name: "ReactJS",
+      icon: <SiReact className="text-tech-cpp" />,
+    },
+    {
+      name: "NextJS",
+      icon: <TbBrandNextjs className="text-black" />,
+    },
+    {
+      name: "Tailwind",
+      icon: <SiTailwindcss className="text-tech-tailwind" />,
+    },
+    {
+      name: "Flask",
+      icon: <SiFlask className="text-black" />,
+    },
+    {
+      name: "Framer Motion",
+      icon: <TbBrandFramerMotion className="text-black" />,
+    },
+    {
+      name: "Firebase",
+      icon: <SiFirebase className="text-tech-firebase" />,
+    },
+    {
+      name: "Prisma",
+      icon: <SiPrisma className="text-tech-prisma" />,
+    },
+    {
+      name: "Cypress.io",
+      icon: <SiCypress className="text-tech-cypress" />,
+    },
+    {
+      name: "Postman",
+      icon: <SiPostman className="text-tech-postman" />,
+    },
+  ],
+  Design: [
+    { name: "Figna", icon: <SiFigma className="text-tech-figma" /> },
+    { name: "Adobe", icon: <SiAdobe className="text-red-500" /> },
+  ],
+};
 
 // import React, { useState } from "react";
 // import { skill } from "@/data/icons";
--- src/data/icons.js
+++ src/data/icons.js
@@ -1,46 +1,3 @@
-// export const icons = [
-//   {
-//     language: "C++",
-//     badgeUrl:
-//       "https://img.shields.io/badge/C++-%2300599C.svg?style=for-the-badge&logo=c%2B%2B&logoColor=white",
-//   },
-//   {
-//     language: "C#",
-//     badgeUrl:
-//       "https://img.shields.io/badge/C%23-%23239120.svg?style=for-the-badge&logo=c-sharp&logoColor=white",
-//   },
-//   {
-//     language: "Python",
-//     badgeUrl:
-//       "https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white",
-//   },
-//   {
-//     language: "HTML",
-//     badgeUrl:
-//       "https://img.shields.io/badge/HTML5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white",
-//   },
-//   {
-//     language: "CSS",
-//     badgeUrl:
-//       "https://img.shields.io/badge/CSS3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white",
-//   },
-//   {
-//     language: "JavaScript",
-//     badgeUrl:
-//       "https://img.shields.io/badge/JavaScript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E",
-//   },
-//   {
-//     language: "Java",
-//     badgeUrl:
-//       "https://img.shields.io/badge/Java-%23ED8B00.svg?style=for-the-badge&logo=java&logoColor=white",
-//   },
-//   {
-//     language: "MySQL",
-//     badgeUrl:
-//       "https://img.shields.io/badge/MySQL-00000F?style=for-the-badge&logo=mysql&logoColor=white",
-//   },
-// ];
-
 import { SiJavascript } from "react-icons/si";
 import { TbBrandNextjs } from "react-icons/tb";
 import { DiJava } from "react-icons/di";
@@ -65,7 +22,7 @@ import { SiAdobe } from "react-icons/si";
 // import { SiOpenai } from "react-icons/si";
 // import { SiGoogleanalytics } from "react-icons/si";
 
-const skills = {
+const SKILLS = {
   Programming: [
     {
       name: "C++",
@@ -137,4 +94,4 @@ const skills = {
   ],
 };
 
-export default skills;
+export default SKILLS;
